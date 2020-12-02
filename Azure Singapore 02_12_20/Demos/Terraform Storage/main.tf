c44cffcf8072d4120f07b789254cfdda1acb8631
Sam Cogan <mail@samcogan.com>
2020-12-02 12:54:13 +0000

    add azure singapore

--- Azure Singapore 02_12_20/Demos/Terraform Storage/main.tf
+++ Azure Singapore 02_12_20/Demos/Terraform Storage/main.tf
@@ -10,11 +10,11 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "devopsdemo003"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "staging"
